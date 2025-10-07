c79c3e3b5f7bfa136551375f328b2b6ed422f0b7
irshad0429 <121759212+irshad0429@users.noreply.github.com>
2025-10-07 23:08:21 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState("Enter text here")
+    const [text, setText] = useState("")
 
     const handleOnClick1 = () => {
         setText(text.toUpperCase());
@@ -12,39 +12,28 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const [text1, setText1] = useState("Enter text here")
-
     const handleOnClick2 = () => {
-        setText1(text.toLowerCase());
+        setText(text.toLowerCase());
     }
 
-    const handleOnChange2 = (event) => {
-        setText1(event.target.value)
-    }
-    
     return (
         <div>
-
             <div>
-                <h1>This Section Is Used For Lower to Upper Text</h1>
+                <h1>This Web Is Used For Lower to Upper And Upper to Lower Text</h1>
                 <div>
                     <h4>{props.heading}</h4>
                     <div className='mb-3'>
                         <textarea className='form-control' name="text" id="myBox" rows="6" value={text} onChange={handleOnChange1} ></textarea>
                     </div>
-                    <button className='btn btn-primary' onClick={handleOnClick1}>Change</button>
+                    <button className='btn btn-primary' onClick={handleOnClick1}>Upper Case</button>
+                    <button className='btn btn-primary mx-2' onClick={handleOnClick2}>Lower Case</button>
                 </div>
             </div>
-
-            <div>
-                <h1>This Section Is Used For Upper to Lower Text</h1>
-                <div>
-                    <h4>{props.heading}</h4>
-                    <div className='mb-3'>
-                        <textarea className='form-control' name="text" id="myBox" rows="6" value={text1} onChange={handleOnChange2} ></textarea>
-                    </div>
-                    <button className='btn btn-primary' onClick={handleOnClick2}>Change</button>
-                </div>
+            <div className='container mt-2'>
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes to read this</p>
+                <p>{text}</p>
             </div>
         </div>
     )
